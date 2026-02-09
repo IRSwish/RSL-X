aee4a5788db79c6a4ee5f4a3ba2d20b3e2681c1b
IRSwish <48881467+IRSwish@users.noreply.github.com>
2026-02-09 11:15:25 +0100

    ad update

--- config/site-config.js
+++ config/site-config.js
@@ -3,6 +3,6 @@ window.siteConfig = {
   adBanner: {
     enabled: true,
     imageUrl: "/Ad_Banner.webp",
-    link: "https://pl.go-ga.me/xnlzldre"
+    link: "https://pl.go-ga.me/jf4wgda0"
   }
 };
